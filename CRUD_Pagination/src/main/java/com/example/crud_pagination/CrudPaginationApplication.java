4254541b7d9687faaa7df8f3292b9d8b5be96413
A39384 <75817960+A39384@users.noreply.github.com>
2023-10-11 23:56:20 +0700

    crud

---
+++ CRUD_Pagination/src/main/java/com/example/crud_pagination/CrudPaginationApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_pagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPaginationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudPaginationApplication.class, args);
+    }
+
+}
---
+++ CRUD_Tutorial/src/main/java/com/example/CRUD_Tutorial/CRUD_TutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUD_Tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CRUD_TutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CRUD_TutorialApplication.class, args);
+    }
+
+}
